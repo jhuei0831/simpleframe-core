38dd1765f97c672c8104e9274cbcd6c6e7e27c4b
Jhuei <jhuei0831@ntnu.edu.tw>
2021-08-18 17:00:43 +0800

    edit config

--- src/config.php
+++ src/config.php
@@ -9,9 +9,9 @@
         /**
          * 網站完整地址
          *
-         * @return void
+         * @return string
          */
-        public function app_address()
+        public function app_address(): string
         {
             $address = $this->app_portocol()."://".$this->app_domain()."/".$this->app_folder().$this->app_name()."/";
             return $address;
@@ -20,9 +20,9 @@
         /**
          * 網站網域
          *
-         * @return void
+         * @return string
          */
-        protected function app_domain()
+        protected function app_domain(): string
         {
             $domain = isset($_SERVER["HTTP_HOST"]) ? $_SERVER["HTTP_HOST"] : "localhost";
             return $domain;
@@ -31,10 +31,13 @@
         /**
          * 網站子資料夾
          *
-         * @return void
+         * @return string
          */
-        protected function app_folder()
+        protected function app_folder(): string
         {
+            if (!isset($_ENV['APP_FOLDER'])) {
+                throw new Exception("Please defined APP_FOLDER in .env", 1);
+            }
             $folder = isset($_ENV['APP_FOLDER']) ? $_ENV['APP_FOLDER'] : '';
             return $folder;
         }
@@ -42,10 +45,13 @@
         /**
          * 網站名稱
          *
-         * @return void
+         * @return string
          */
-        protected function app_name()
+        protected function app_name(): string
         {
+            if (!isset($_ENV['APP_NAME'])) {
+                throw new Exception("Please defined APP_NAME in .env", 1);
+            }
             $name = isset($_ENV['APP_NAME']) ? $_ENV['APP_NAME'] : '';
             return $name;
         }
@@ -53,22 +59,29 @@
         /**
          * 網站協定
          *
-         * @return void
+         * @return string
          */
-        protected function app_portocol()
+        protected function app_portocol(): string
         {
             $portocol = isset($_SERVER["REQUEST_SCHEME"]) ? $_SERVER["REQUEST_SCHEME"] : "http";
             return $portocol;
         }
-
-        public function csrf_token()
+        
+        /**
+         * csrf_token
+         *
+         * @return string
+         */
+        public function csrf_token(): string
         {
-            try {
-                $token = $_SESSION['token'];
-                return $token;
-            } catch (Exception $e) {
+            if (session_status() === PHP_SESSION_NONE) {
+                session_start();
+            }
+            if (!isset($_SESSION['token'])) {
                 throw new Exception("Please defined SESSION token", 1);
             }
+            $token = $_SESSION['token'];
+            return $token;
         }
         
         /**
@@ -76,9 +89,12 @@
          *
          * @return void
          */
-        public function is_debug()
+        public function is_debug(): string
         {
-            $debug = isset($_SERVER["IS_DEBUG"]) ? $_SERVER["IS_DEBUG"] : "FALSE";
+            if (!isset($_ENV['IS_DEBUG'])) {
+                throw new Exception("Please defined IS_DEBUG in .env", 1);
+            }
+            $debug = isset($_ENV["IS_DEBUG"]) ? $_ENV["IS_DEBUG"] : "FALSE";
             return $debug;
         }
     }
