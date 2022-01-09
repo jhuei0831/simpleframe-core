2e4063462fae68549059f376b7f7a24c00804443
jhuei0831 <57594444+jhuei0831@users.noreply.github.com>
2022-01-09 18:41:06 +0800

    修改BreadCrumb網址

--- src/Support/Toolbox.php
+++ src/Support/Toolbox.php
@@ -49,7 +49,7 @@
                         <svg class='flex-shrink-0 w-6 h-full text-gray-200' viewBox='0 0 24 44' preserveAspectRatio='none' fill='currentColor' xmlns='http://www.w3.org/2000/svg' aria-hidden='true'>
                             <path d='M.293 0l22 22-22 22h1.414l22-22-22-22H.293z' />
                         </svg>
-                        <a href='{$url}' class='".($url=='#'?'pointer-events-none text-cyan-600':'text-gray-500')." ml-4 text-sm font-medium hover:text-gray-700'>{$name}</a>
+                        <a href='{$url}/' class='".($url=='#'?'pointer-events-none text-cyan-600':'text-gray-500')." ml-4 text-sm font-medium hover:text-gray-700'>{$name}</a>
                     </div>
                 </li>";
             }
